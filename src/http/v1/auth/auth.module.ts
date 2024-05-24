39003ab603d783708c7794815b22fd0f808697d9
yanhf <yanhairulfirdan@gmail.com>
2024-05-24 08:45:54 +0800

    make auth guard

--- src/http/v1/auth/auth.module.ts
+++ src/http/v1/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from "@nestjs/common";
 import { UserModule } from "src/user/user.module";
 import RegisterController from "./register/register.controller";
+import LoginController from "./login/login.controller";
+import { APP_GUARD } from "@nestjs/core";
+import AuthGuard from "./guards/auth.guard";
 
 @Module({
     imports: [UserModule],
-    controllers: [RegisterController],
+    controllers: [
+        RegisterController,
+        LoginController,
+    ],
+    providers: [
+        {
+          provide: APP_GUARD,
+          useClass: AuthGuard,
+        }
+      ],
 })
 export class AuthModule {}
