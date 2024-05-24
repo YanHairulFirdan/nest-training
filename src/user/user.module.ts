139863865a098a8fa76d758c6afb9a7381cce164
yanhf <yanhairulfirdan@gmail.com>
2024-05-24 08:46:06 +0800

    implement login API

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import UserService from './user.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+    imports: [
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: '1d' },
+          })
+    ],
     providers: [UserService],
     exports: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,9 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
 import { CustomPrismaService } from "nestjs-prisma";
 import { ExtendedPrismaClient } from "src/prisma/extension";
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { User } from "@prisma/client";
 
 type CreateUser = {
   email: string,
@@ -9,15 +11,57 @@ type CreateUser = {
   name: string
 }
 
+type RegisterUser = {
+  email: string,
+  password: string,
+}
+
+type LoginUser = {
+  access_token: string;
+}
+
 @Injectable()
 export default class UserService {
   @Inject('PrismaService')
   private prisma: CustomPrismaService<ExtendedPrismaClient>
 
+  @Inject(JwtService)
+  private jwtService: JwtService;
+
   async createUser(data: CreateUser) {
     data.password = await this.hashPassword(data.password);
 
     return this.prisma.client.user.create({ data });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.prisma.client.user.findUnique({
+      where: { email },
+    })
+  }
+
+  async login(data: RegisterUser): Promise<string> {
+    const user = await this.findByEmail(data.email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const passwordMatch = await bcrypt.compare(data.password, user.password);
+
+    if (!passwordMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return this.jwtService.sign({id: user.id, email: user.email});
+  }
+
+  async verifyToken(token: string): Promise<User | null> {
+    const payload = await this.jwtService.verifyAsync(token, {
+      secret: process.env.JWT_SECRET,
+    });
+
+    return await this.findByEmail(payload.email);
   }
 
   private async hashPassword(password: string): Promise<string> {
