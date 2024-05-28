6c33579eb2b164eea32be4ead8ff26e6fab5da6b
yanhf <yanhairulfirdan@gmail.com>
2024-05-28 11:32:00 +0800

    use global filter for validation exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import ValidationExceptionFilter from './validation-rule/filters/validation-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
   })
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new ValidationExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
