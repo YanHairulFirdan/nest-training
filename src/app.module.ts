908ad98a819efa07bdfd4831813fc8d583b83a06
yanhf <yanhairulfirdan@gmail.com>
2024-05-28 11:31:17 +0800

    register auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { CustomPrismaModule } from 'nestjs-prisma';
 import { extendedPrismaClient } from './prisma/extension';
 import { ValidationRuleModule } from './validation-rule/validation-rule.module';
-import { HTTPModule } from './http/controllers/v1/http.module';
-import { RouterModule } from '@nestjs/core';
+import { HTTPModule } from './http/http.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +14,7 @@ import { RouterModule } from '@nestjs/core';
       },
       isGlobal: true,
     }), 
+    AuthModule,
     ValidationRuleModule,
     HTTPModule, 
   ],
