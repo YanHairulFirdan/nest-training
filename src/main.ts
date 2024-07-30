9d0f506eb010ef3b6411895878243f05ad13b51f
yanhf <yanhairulfirdan@gmail.com>
2024-07-30 11:59:03 +0800

    load env variable using config module

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,22 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import ValidationExceptionFilter from './validation-rule/filters/validation-exception.filter';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
     prefix: 'api/v',
-  })
+  });
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new ValidationExceptionFilter());
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+
+  await app.listen(port);
 }
 bootstrap();
